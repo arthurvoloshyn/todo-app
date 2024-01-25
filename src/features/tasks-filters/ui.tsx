6a965421d5a5c6c53173bec4ba47cdf88a7671d8
arturvoloshyn <arturvoloshyn@gmail.com>
2024-01-25 19:53:50 +0200

    feat(features): add features

---
+++ src/features/tasks-filters/ui.tsx
@@ -0,0 +1,36 @@
+import { reflect } from "@effector/reflect";
+
+import { Radio } from "shared/ui/radio";
+import { taskModel } from "entities/task";
+import { filtersList, getFilterById, DEFAULT_FILTER } from "./config";
+
+type Props = {
+  loading: boolean;
+  onFilterClick: (p: taskModel.QueryConfig) => void;
+};
+
+const View = ({ loading, onFilterClick }: Props) => {
+  return (
+    <Radio.Group defaultValue={DEFAULT_FILTER} buttonStyle="solid">
+      {filtersList.map(({ title, id }) => (
+        <Radio.Button
+          key={id}
+          onClick={() => onFilterClick(getFilterById(id).config)}
+          value={id}
+          disabled={loading}
+        >
+          {title}
+        </Radio.Button>
+      ))}
+    </Radio.Group>
+  );
+};
+
+// The use of effector-reflect here is optional and not critical within the methodology
+export const TasksFilters = reflect({
+  view: View,
+  bind: {
+    loading: taskModel.$tasksListLoading,
+    onFilterClick: taskModel.events.setQueryConfig,
+  },
+});
