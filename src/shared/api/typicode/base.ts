44665e71cf1dca68570c36d532215cb16b483eea
arturvoloshyn <arturvoloshyn@gmail.com>
2024-01-25 19:52:43 +0200

    feat(shared/ui): add shared/ui modules

---
+++ src/shared/api/typicode/base.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { API_URL } from "shared/config";
+
+// Potentially, you could pass an accessToken
+export const apiInstance = axios.create({
+    baseURL: API_URL
+});
