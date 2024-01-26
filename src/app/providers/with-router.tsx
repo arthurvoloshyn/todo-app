e5c22685b8f272cad9f998a7f497b7e835ccb8ad
arturvoloshyn <arturvoloshyn@gmail.com>
2024-01-26 23:55:38 +0200

    refactor(shared/config): rename basename

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { basename } from "shared/config";
+import { basePath } from "shared/config";
 import { Spin } from "shared/ui";
 
 export const withRouter = (component: () => React.ReactNode) => () => (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename={basePath}>
         <Suspense fallback={<Spin delay={300} className="overlay" size="large" />}>
             {component()}
         </Suspense>
